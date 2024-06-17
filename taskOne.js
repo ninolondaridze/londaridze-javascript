64c1a2ea7c04a0f609613b5594c3ff8dab633cb0
Nino Londaridze <nino_londaridze@epam.com>
2024-06-17 15:14:59 +0400

    AQA test task v1

---
+++ taskOne.js
@@ -0,0 +1,27 @@
+function checkNumber (number) {
+  if (number > 7) {
+    console.log ('Hello');
+  }
+}
+
+const number = prompt('Enter a number:');
+checkNumber(number);
+
+function checkName (name) {
+  if (name === 'John') {
+    console.log('Hello John')
+  } else {
+    console.log('There is no such name')
+  }
+}
+
+const input = prompt("What's your name?");
+checkName(input);
+
+function multiplesOfThree (arr) {
+  return arr.filter(x => x % 3 === 0);
+}
+
+const arrayInput = prompt("Enter numbers separated by spaces");
+const array = arrayInput.split(' ').map(Number);
+console.log('Multiples of 3 are:', multiplesOfThree(array));
